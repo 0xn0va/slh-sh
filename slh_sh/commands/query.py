7914ee886efcb6e7f1e48b2e776ce91e26d1e396
Nova <2989791+0xn0va@users.noreply.github.com>
2023-11-19 14:05:36 +0200

    query theme subtheme formatting

--- slh_sh/commands/query.py
+++ slh_sh/commands/query.py
@@ -7,14 +7,13 @@ import pyperclip
 from rich import print
 from typing_extensions import Annotated
 
-# from scholarly import scholarly
 from slh_sh.utils.file import get_conf
 
 app = typer.Typer()
 
 
 @app.command()
-def themes(
+def query(
     terms: Annotated[
         list[str],
         typer.Argument(
@@ -23,12 +22,111 @@ first argument one or more Author or ID separated by colon, second ThemeName, th
 e.g. slh-sh query themes John,120,192 Stage_1 Derogation"""
         ),
     ],
-    studytable: Annotated[str, typer.Option("-st", "--studytable", help="Study table name")] = get_conf("default_studies"),
-    idname: Annotated[str, typer.Option("-id", "--idname", help="ID column name")] = get_conf("default_id"),
+    studytable: Annotated[
+        str, typer.Option("-st", "--studytable", help="Study table name")
+    ] = get_conf("default_studies"),
+    idname: Annotated[
+        str, typer.Option("-id", "--idname", help="ID column name")
+    ] = get_conf("default_id"),
     copy: Annotated[
         bool, typer.Option("-c", "--copy", help="Copy to clipboard")
     ] = False,
 ):
     """Get themes about a study from a database table by ID."""
 
-    print(terms)
+    authors = terms[0].split(",")
+    theme = ""
+    subtheme = ""
+    if len(terms) == 2:
+        theme = terms[1]
+    if len(terms) == 3:
+        subtheme = terms[2]
+    if len(terms) > 3:
+        print("Too many arguments!")
+        exit()
+
+    results = {}
+
+    conn = sql.connect(get_conf("sqlite_db"))
+    curr = conn.cursor()
+
+    for author in authors:
+        if author.isnumeric():
+            curr.execute(f"SELECT * FROM {studytable} WHERE {idname} = {author}")
+            db_res = curr.fetchone()
+            if db_res == "None":
+                print(
+                    f"Study with the ID: {author} not found in database table: {studytable}!"
+                )
+            else:
+                curr.execute(
+                    f"SELECT citation, bibliography FROM studies WHERE {get_conf('default_id')} = {author}"
+                )
+                db_citbib = curr.fetchone()
+                res = {
+                    "ID": author,
+                    "Authors": db_res[2],
+                    "Title": db_res[1],
+                    "Theme": theme,
+                    "Subtheme": subtheme,
+                    "Citation": db_citbib[0],
+                    "Bibliography": db_citbib[1],
+                }
+                results[author] = res
+        else:
+            curr.execute(f"SELECT * FROM {studytable} WHERE authors LIKE '%{author}%'")
+            db_res = curr.fetchone()
+            if db_res == "None":
+                print(
+                    f"Study with the Author: {author} not found in database table: {studytable}!"
+                )
+            else:
+                id_index = [
+                    i for i, col in enumerate(curr.description) if col[0] == idname
+                ][0]
+                curr.execute(
+                    f"SELECT citation, bibliography FROM studies WHERE {get_conf('default_id')} = {db_res[id_index]}"
+                )
+                db_citbib = curr.fetchone()
+                res = {
+                    "ID": db_res[id_index],
+                    "Authors": db_res[2],
+                    "Title": db_res[1],
+                    "Theme": theme,
+                    "Subtheme": subtheme,
+                    "Citation": db_citbib[0],
+                    "Bibliography": db_citbib[1],
+                }
+                results[author] = res
+    if theme != "":
+        for key, value in results.items():
+            if subtheme == "":
+                curr.execute(f"SELECT * FROM {theme} WHERE {idname} = {value['ID']}")
+                db_res = curr.fetchone()
+                if db_res[0] == "None":
+                    print(
+                        f"Theme for Study with the ID: {value['ID']} not found in database table: {theme}!"
+                    )
+                else:
+                    # remove ID column and None values
+                    db_res = [x for x in db_res if x != "None"]
+                    db_res = [x for x in db_res if x != value["ID"]]
+                    # add column names
+                    db_res = dict(zip(curr.description[1:], db_res))
+                    # remove empty columns
+                    db_res = {k: v for k, v in db_res.items() if v != ""}
+                    value["Theme_Text"] = db_res
+            else:
+                curr.execute(
+                    f"SELECT {subtheme} FROM {theme} WHERE {idname} = {value['ID']}"
+                )
+                db_res = curr.fetchone()
+                if db_res[0] == "None":
+                    print(
+                        f"Subtheme for Study with the ID: {value['ID']} not found in database table: {theme}!"
+                    )
+                else:
+                    value["Subtheme_Text"] = db_res[0]
+
+    conn.close()
+    print(results)
--- slh_sh/main.py
+++ slh_sh/main.py
@@ -6,7 +6,8 @@ import slh_sh.commands.add as add
 import slh_sh.commands.go as go
 import slh_sh.commands.sync as sync
 import slh_sh.commands.self as self
-import slh_sh.commands.query as query
+
+from slh_sh.commands.query import query
 
 app = typer.Typer()
 
@@ -16,7 +17,8 @@ app.add_typer(add.app, name="add")
 app.add_typer(go.app, name="go")
 app.add_typer(sync.app, name="sync")
 app.add_typer(self.app, name="self")
-app.add_typer(query.app, name="query")
+
+app.command()(query)
 
 if __name__ == "__main__":
     app()
